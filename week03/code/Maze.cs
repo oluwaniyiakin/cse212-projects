dfe91ddce3a5f7b5c69616ee005442a81b1b5b4f
OLUWANIYIAKIN <olu24002@byui.edu>
2025-06-03 18:59:48 +0100

    week 5

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,47 +1,17 @@
 using System;
 using System.Collections.Generic;
 
-/// <summary>
-/// Defines a maze using a dictionary. The dictionary is provided by the
-/// user when the Maze object is created. The dictionary will contain the
-/// following mapping:
-///
-/// (x,y) : [left, right, up, down]
-///
-/// 'x' and 'y' are integers and represent locations in the maze.
-/// 'left', 'right', 'up', and 'down' are booleans representing valid directions.
-///
-/// If a direction is false, then there is a wall in that direction.
-/// If a direction is true, then movement is allowed.
-///
-/// If there is a wall, an InvalidOperationException with the message
-/// "Can't go that way!" is thrown. Otherwise, the current position
-/// (_currX, _currY) is updated.
-/// </summary>
 public class Maze
 {
-    // Maze dictionary: keys are coordinates (x, y),
-    // values are bool arrays representing [left, right, up, down]
     private readonly Dictionary<(int x, int y), bool[]> _mazeMap;
-
-    // Current player location coordinates initialized to (1,1)
     private int _currX = 1;
     private int _currY = 1;
 
-    /// <summary>
-    /// Constructor takes a maze dictionary mapping coordinates to
-    /// movement options (left, right, up, down).
-    /// </summary>
-    /// <param name="mazeMap">Dictionary defining the maze</param>
     public Maze(Dictionary<(int x, int y), bool[]> mazeMap)
     {
         _mazeMap = mazeMap ?? throw new ArgumentNullException(nameof(mazeMap));
     }
 
-    /// <summary>
-    /// Checks if you can move left from the current location.
-    /// If allowed, updates the location. Otherwise, throws an exception.
-    /// </summary>
     public void MoveLeft()
     {
         var currentLocation = (_currX, _currY);
@@ -49,14 +19,13 @@ public class Maze
         if (!_mazeMap.ContainsKey(currentLocation))
             throw new InvalidOperationException("Invalid current location in maze.");
 
-        // Check if movement left is allowed from current location
         if (_mazeMap[currentLocation][0])
         {
             var nextLocation = (_currX - 1, _currY);
             if (_mazeMap.ContainsKey(nextLocation))
                 _currX--;
             else
-                throw new InvalidOperationException("Can't go that way (no cell)!");
+                throw new InvalidOperationException("Can't go that way!");
         }
         else
         {
@@ -64,10 +33,6 @@ public class Maze
         }
     }
 
-    /// <summary>
-    /// Checks if you can move right from the current location.
-    /// If allowed, updates the location. Otherwise, throws an exception.
-    /// </summary>
     public void MoveRight()
     {
         var currentLocation = (_currX, _currY);
@@ -81,7 +46,7 @@ public class Maze
             if (_mazeMap.ContainsKey(nextLocation))
                 _currX++;
             else
-                throw new InvalidOperationException("Can't go that way (no cell)!");
+                throw new InvalidOperationException("Can't go that way!");
         }
         else
         {
@@ -89,10 +54,6 @@ public class Maze
         }
     }
 
-    /// <summary>
-    /// Checks if you can move up from the current location.
-    /// If allowed, updates the location. Otherwise, throws an exception.
-    /// </summary>
     public void MoveUp()
     {
         var currentLocation = (_currX, _currY);
@@ -106,7 +67,7 @@ public class Maze
             if (_mazeMap.ContainsKey(nextLocation))
                 _currY++;
             else
-                throw new InvalidOperationException("Can't go that way (no cell)!");
+                throw new InvalidOperationException("Can't go that way!");
         }
         else
         {
@@ -114,10 +75,6 @@ public class Maze
         }
     }
 
-    /// <summary>
-    /// Checks if you can move down from the current location.
-    /// If allowed, updates the location. Otherwise, throws an exception.
-    /// </summary>
     public void MoveDown()
     {
         var currentLocation = (_currX, _currY);
@@ -131,7 +88,7 @@ public class Maze
             if (_mazeMap.ContainsKey(nextLocation))
                 _currY--;
             else
-                throw new InvalidOperationException("Can't go that way (no cell)!");
+                throw new InvalidOperationException("Can't go that way!");
         }
         else
         {
@@ -139,10 +96,6 @@ public class Maze
         }
     }
 
-    /// <summary>
-    /// Returns a string representing the current position in the maze.
-    /// </summary>
-    /// <returns>String status of current location</returns>
     public string GetStatus()
     {
         return $"Current location (x={_currX}, y={_currY})";
--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 
 public static class Recursion
 {
@@ -7,15 +8,16 @@ public static class Recursion
     /// # Problem 1 #
     /// #############
     /// Using recursion, find the sum of 1^2 + 2^2 + 3^2 + ... + n^2
-    /// and return it.  Remember to both express the solution 
-    /// in terms of recursive call on a smaller problem and 
-    /// to identify a base case (terminating case).  If the value of
-    /// n <= 0, just return 0.   A loop should not be used.
+    /// and return it.
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        // Base case
+        if (n <= 0)
+            return 0;
+
+        // Recursive case
+        return (n * n) + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -23,121 +25,107 @@ public static class Recursion
     /// # Problem 2 #
     /// #############
     /// Using recursion, insert permutations of length
-    /// 'size' from a list of 'letters' into the results list.  This function
-    /// should assume that each letter is unique (i.e. the 
-    /// function does not need to find unique permutations).
-    ///
-    /// In mathematics, we can calculate the number of permutations
-    /// using the formula: len(letters)! / (len(letters) - size)!
-    ///
-    /// For example, if letters was [A,B,C] and size was 2 then
-    /// the following would the contents of the results array after the function ran: AB, AC, BA, BC, CA, CB (might be in 
-    /// a different order).
-    ///
-    /// You can assume that the size specified is always valid (between 1 
-    /// and the length of the letters list).
+    /// 'size' from a list of 'letters' into the results list.
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            // Skip letters already used in the current word
+            if (!word.Contains(letters[i]))
+            {
+                PermutationsChoose(results, letters, size, word + letters[i]);
+            }
+        }
     }
 
     /// <summary>
     /// #############
     /// # Problem 3 #
     /// #############
-    /// Imagine that there was a staircase with 's' stairs.  
-    /// We want to count how many ways there are to climb 
-    /// the stairs.  If the person could only climb one 
-    /// stair at a time, then the total would be just one.  
-    /// However, if the person could choose to climb either 
-    /// one, two, or three stairs at a time (in any order), 
-    /// then the total possibilities become much more 
-    /// complicated.  If there were just three stairs,
-    /// the possible ways to climb would be four as follows:
-    ///
-    ///     1 step, 1 step, 1 step
-    ///     1 step, 2 step
-    ///     2 step, 1 step
-    ///     3 step
-    ///
-    /// With just one step to go, the ways to get
-    /// to the top of 's' stairs is to either:
-    ///
-    /// - take a single step from the second to last step, 
-    /// - take a double step from the third to last step, 
-    /// - take a triple step from the fourth to last step
-    ///
-    /// We don't need to think about scenarios like taking two 
-    /// single steps from the third to last step because this
-    /// is already part of the first scenario (taking a single
-    /// step from the second to last step).
-    ///
-    /// These final leaps give us a sum:
-    ///
-    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
-    ///                       CountWaysToClimb(s-2) +
-    ///                       CountWaysToClimb(s-3)
-    ///
-    /// To run this function for larger values of 's', you will need
-    /// to update this function to use memoization.  The parameter
-    /// 'remember' has already been added as an input parameter to 
-    /// the function for you to complete this task.
+    /// Count the number of ways to climb s stairs taking
+    /// 1, 2, or 3 steps at a time using recursion with memoization.
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
-        // Base Cases
-        if (s == 0)
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
+
+        if (s < 0)
             return 0;
-        if (s == 1)
+        if (s == 0)
             return 1;
-        if (s == 2)
-            return 2;
-        if (s == 3)
-            return 4;
 
-        // TODO Start Problem 3
+        if (remember.ContainsKey(s))
+            return remember[s];
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
+        decimal total =
+            CountWaysToClimb(s - 1, remember) +
+            CountWaysToClimb(s - 2, remember) +
+            CountWaysToClimb(s - 3, remember);
+
+        remember[s] = total;
+        return total;
     }
 
     /// <summary>
     /// #############
     /// # Problem 4 #
     /// #############
-    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
-    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
-    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
-    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
-    /// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
-    ///	
-    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
-    /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
+    /// Generate all binary strings that match a pattern with wildcards (*).
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        // Replace the '*' with '0' and '1' and recurse
+        WildcardBinary(pattern.Substring(0, index) + "0" + pattern.Substring(index + 1), results);
+        WildcardBinary(pattern.Substring(0, index) + "1" + pattern.Substring(index + 1), results);
     }
 
     /// <summary>
-    /// Use recursion to insert all paths that start at (0,0) and end at the
-    /// 'end' square into the results list.
+    /// #############
+    /// # Problem 5 #
+    /// #############
+    /// Recursively solve the maze from (0,0) to endpoint (value 2).
     /// </summary>
-    public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
+    public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<(int, int)>? currPath = null)
     {
-        // If this is the first time running the function, then we need
-        // to initialize the currPath list.
-        if (currPath == null) {
-            currPath = new List<ValueTuple<int, int>>();
+        if (currPath == null)
+            currPath = new List<(int, int)>();
+
+        // Out of bounds or wall or visited
+        if (!maze.InBounds(x, y) || maze.Get(x, y) == 0 || currPath.Contains((x, y)))
+            return;
+
+        currPath.Add((x, y));
+
+        // Reached the goal
+        if (maze.Get(x, y) == 2)
+        {
+            results.Add(currPath.AsString());
+            currPath.RemoveAt(currPath.Count - 1); // Backtrack
+            return;
         }
-        
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
 
-        // TODO Start Problem 5
-        // ADD CODE HERE
+        // Explore all four directions
+        SolveMaze(results, maze, x + 1, y, currPath);
+        SolveMaze(results, maze, x - 1, y, currPath);
+        SolveMaze(results, maze, x, y + 1, currPath);
+        SolveMaze(results, maze, x, y - 1, currPath);
 
-        // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+        // Backtrack
+        currPath.RemoveAt(currPath.Count - 1);
     }
 }
